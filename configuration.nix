87ef6648e76f8464bd15612995e0a82a4921bac4
Tom Vincent <git@tlvince.com>
2023-11-12 15:16:00 +0000

    feat(tlp): power profiles

--- configuration.nix
+++ configuration.nix
@@ -162,6 +162,18 @@
 
   services.power-profiles-daemon.enable = false;
   services.tlp.enable = true;
+  services.tlp.settings = {
+    # active by default in kernel >=6.5
+    CPU_DRIVER_OPMODE_ON_AC = "active";
+    CPU_DRIVER_OPMODE_ON_BAT = "active";
+    CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+    CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+    # powersave required for EPP to work
+    CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    PLATFORM_PROFILE_ON_AC = "balanced";
+    PLATFORM_PROFILE_ON_BAT = "low-power";
+  };
 
   services.pipewire = {
     enable = true;
