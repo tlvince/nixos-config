6245289bb5ad05c2ca64c61442945eb562492725
Tom Vincent <git@tlvince.com>
2023-12-21 11:31:13 +0000

    ci: add compare URLs

---
+++ .github/scripts/compare.js
@@ -0,0 +1,38 @@
+const generateCompareLinks = (flakeUpdateOutput) => {
+  const regex =
+    /'github:(?<repo>[^/]+\/[^/]+)\/(?<oldCommit>[0-9a-f]+)'.*→ 'github:\1\/(?<newCommit>[0-9a-f]+)'/g;
+
+  let match;
+  const links = [];
+
+  while ((match = regex.exec(flakeUpdateOutput)) !== null) {
+    const { repo, oldCommit, newCommit } = match.groups;
+    const compareUrl = `https://github.com/${repo}/compare/${oldCommit}...${newCommit}`;
+    links.push(compareUrl);
+  }
+
+  return links;
+};
+
+module.exports = async ({ github, context, core }) => {
+  const commits = await github.repos.listCommits({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    per_page: 1,
+    page: 1,
+  });
+
+  const commit = commits?.data?.[0]?.commit?.message;
+  if (!commit) {
+    core.warning("unable to determine latest commit message");
+    return;
+  }
+
+  const compareLinks = generateCompareLinks(commit);
+  if (!compareLinks.length) {
+    core.warning("no compare links found");
+    return;
+  }
+
+  return compareLinks;
+};
