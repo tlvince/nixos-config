7085b9b89f8a467c75abe8abbd09ad4f88ef12ab
Tom Vincent <git@tlvince.com>
2024-02-09 15:09:38 +0000

    fix: reduce boot entries

--- configuration.nix
+++ configuration.nix
@@ -23,7 +23,7 @@
   boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.lanzaboote = {
     enable = true;
-    configurationLimit = 10;
+    configurationLimit = 5;
     pkiBundle = "/etc/secureboot";
   };
 
