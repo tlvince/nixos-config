1141c6d5fbced52380651826abde748538f6ce1a
Tom Vincent <git@tlvince.com>
2024-08-23 13:44:16 +0100

    fix(gdm): rm frpint PAM override

--- configuration.nix
+++ configuration.nix
@@ -211,27 +211,6 @@
   programs.firefox.enable = true;
   programs.zsh.enable = true;
 
-  # Workaround for gdm
-  # See: https://github.com/NixOS/nixpkgs/issues/171136
-  # See: https://github.com/NixOS/nixpkgs/pull/171140
-  security.pam.services.login.fprintAuth = false;
-  security.pam.services.gdm-fingerprint = lib.mkIf (config.services.fprintd.enable) {
-    text = ''
-      auth       required                    pam_shells.so
-      auth       requisite                   pam_nologin.so
-      auth       requisite                   pam_faillock.so      preauth
-      auth       required                    ${pkgs.fprintd}/lib/security/pam_fprintd.so
-      auth       optional                    pam_permit.so
-      auth       required                    pam_env.so
-      auth       [success=ok default=1]      ${pkgs.gnome.gdm}/lib/security/pam_gdm.so
-      auth       optional                    ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so
-      account    include                     login
-      password   required                    pam_deny.so
-      session    include                     login
-      session    optional                    ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
-    '';
-  };
-
   services.btrbk.instances = {
     btrbk = {
       # Timer disabled in favour of hourly & daily timers
