857a0a31d815852dbb62e82ae947882dcee54450
Tom Vincent <git@tlvince.com>
2025-01-15 10:53:37 +0000

    import

--- cm3588.nix
+++ cm3588.nix
@@ -8,7 +8,7 @@
   imports = [
     (modulesPath + "/profiles/headless.nix")
     (modulesPath + "/profiles/minimal.nix")
-    "./modules/librespot.nix"
+    ./modules/librespot.nix
   ];
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
