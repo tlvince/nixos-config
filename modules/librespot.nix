5f2de8b2c602d3a31415858e147db86420105e42
Tom Vincent <git@tlvince.com>
2025-01-15 10:58:06 +0000

    lint

--- modules/librespot.nix
+++ modules/librespot.nix
@@ -6,8 +6,7 @@
   systemd.services.librespot = {
     after = ["network.target" "sound.target"];
     description = "Librespot (an open source Spotify client)";
-    documentation = "https://github.com/librespot-org/librespot";
-    documentation = "https://github.com/librespot-org/librespot/wiki/Options";
+    documentation = ["https://github.com/librespot-org/librespot/wiki/Options"];
     wantedBy = ["multi-user.target"];
     wants = ["network.target" "sound.target"];
     script = "${pkgs.librespot}/bin/librespot \"%p@%H\"";
