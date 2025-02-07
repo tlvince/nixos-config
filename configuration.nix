e0e87dc0d8b2e5ce2bf230d1bcfad7ae4f89522b
Tom Vincent <git@tlvince.com>
2025-02-07 15:15:51 +0000

    fix: give precedence to default cache

--- configuration.nix
+++ configuration.nix
@@ -212,15 +212,13 @@
       "nix-command"
       "flakes"
     ];
-    substituters = [
+    extra-substituters = [
       "https://tlvince-nixos-config.cachix.org"
       "https://nix-community.cachix.org"
-      "https://cache.nixos.org"
     ];
-    trusted-public-keys = [
+    extra-trusted-public-keys = [
       "tlvince-nixos-config.cachix.org-1:PYVWI+uNlq7mSJxFSPDkkCEtaeQeF4WvjtQKa53ZOyM="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
   };
 
