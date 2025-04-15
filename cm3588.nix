07a145139d8a6b14b3584fde1a2731a88b17604a
Tom Vincent <git@tlvince.com>
2025-04-15 08:41:57 +0100

    fix(cm3588): disable rsa

--- cm3588.nix
+++ cm3588.nix
@@ -139,6 +139,12 @@
   };
   services.openssh = {
     enable = true;
+    hostKeys = [
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
     openFirewall = true;
     settings = {
       AllowGroups = ["wheel"];
