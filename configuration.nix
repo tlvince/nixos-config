90ff9e53504b3159273a03582975576e056f88c3
Tom Vincent <git@tlvince.com>
2025-05-13 09:43:43 +0100

    fix(amdgpu): fix vcn race condition (#262)

--- configuration.nix
+++ configuration.nix
@@ -34,11 +34,11 @@
     kernelPackages = pkgs.linuxPackages_latest;
     kernelPatches = [
       {
-        # https://gitlab.freedesktop.org/mesa/mesa/-/issues/12528#note_2903939
-        name = "drm/amdgpu/vcn: turn-off-doorbell-for-vcn-ring-use";
+        # https://gitlab.freedesktop.org/mesa/mesa/-/issues/12528#note_2906598
+        name = "drm/amdgpu/vcn: fix vcn race condition";
         patch = pkgs.fetchpatch {
-          url ="https://gitlab.freedesktop.org/-/project/176/uploads/1e92f4d6f48df55732cde66a94db1d85/0001-turn-off-doorbell-for-vcn-ring-use.patch";
-          sha256 = "sha256-CSDu90At9rSPGw2p/7u9OSCLbB0jzgMypAOIlUCT1YI=";
+          url = "https://gitlab.freedesktop.org/-/project/176/uploads/defeac39ec232976c7c82aab151bfe63/0001-drm-amdgpu-read-back-DB_CTRL-register-after-write-fo.patch";
+          sha256 = "sha256-cQ0Rz4SaxxzbmIPehZtxrclgp70abBwbWGghyELZnY0=";
         };
       }
     ];
