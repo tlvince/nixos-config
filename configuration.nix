00aa6e4280139ea5312854cd7d13ef6aa04e579f
Tom Vincent <git@tlvince.com>
2025-06-03 13:56:45 +0100

    fixup! fix(framework): bump mt7925

--- configuration.nix
+++ configuration.nix
@@ -221,8 +221,8 @@
             src = pkgs.fetchgit {
               url = "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git";
               rev = "c799f5d3045f2495ceeefcc2b9055ef42843c0e2";
+              hash = "sha256-32uyYuyxIqRVbKrLedhNj2rVsc+bpwvbKKwn5P3mUtg=";
             };
-            outputHash = "sha256-32uyYuyxIqRVbKrLedhNj2rVsc+bpwvbKKwn5P3mUtg=";
           }
         );
       }
