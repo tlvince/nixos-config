0d2e513f487a016f3bdca595678bfa86460b8526
Tom Vincent <git@tlvince.com>
2025-06-28 09:47:52 +0100

    fix(archive): explicit unmount

--- modules/archive.nix
+++ modules/archive.nix
@@ -11,6 +11,7 @@
       systemctl start systemd-cryptsetup@dijkstra.service
       mount -o compress=zstd,noatime /dev/mapper/dijkstra /mnt/dijkstra
       btrbk --config /dev/null archive /mnt/ichbiah/snapshots /mnt/dijkstra/snapshots
+      umount /mnt/dijkstra
       systemctl stop systemd-cryptsetup@dijkstra.service
       sdparm --command=stop --readonly /dev/disk/by-uuid/e14d08e4-7123-4d86-bae0-b3de6f00454f
     '';
