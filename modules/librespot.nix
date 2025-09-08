21ac75763d6e4d08d36315bde87d7a14d76d4fa4
Tom Vincent <git@tlvince.com>
2025-09-08 10:16:47 +0100

    fix(librespot): 0.7.0 (#335)
    Closes #332

--- modules/librespot.nix
+++ modules/librespot.nix
@@ -3,19 +3,6 @@
   pkgs,
   ...
 }: {
-  nixpkgs.overlays = [
-    (
-      final: prev: {
-        # TODO: Use upstream librespot package
-        # Issue URL: https://github.com/tlvince/nixos-config/issues/332
-        # https://github.com/NixOS/nixpkgs/pull/437539
-        # https://github.com/librespot-org/librespot/issues/1527
-        # labels: module:librespot, host:cm3588
-        librespot = final.callPackage ../packages/librespot.nix {};
-      }
-    )
-  ];
-
   systemd.services.librespot = {
     after = ["network.target" "sound.target"];
     description = "Librespot (an open source Spotify client)";
