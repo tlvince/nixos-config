fad93c000a59b637ecfbe09ae3dbe00c97b60232
Tom Vincent <git@tlvince.com>
2025-11-26 14:52:27 +0000

    fix(antigravity): runtime deps

--- modules/antigravity.nix
+++ modules/antigravity.nix
@@ -9,6 +9,17 @@
         gpu
         gui
         network
+        open-urls-in-browser
+        (add-runtime ''
+          SHELL=/bin/sh
+          echo "root:x:0:0:System administrator:/root:$SHELL" > ~/.local/share/jail.nix/passwd
+          echo "$(id -un):x:$(id -u):$(id -g)::$HOME:$SHELL" >> ~/.local/share/jail.nix/passwd
+        '')
+        (add-pkg-deps (with pkgs; [
+          electron
+          nodejs
+          xdg-utils
+        ]))
         (persist-home "antigravity")
         # Add Wayland args
         (set-env "NIXOS_OZONE_WL" "1")
