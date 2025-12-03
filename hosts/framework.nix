c1f23f55411c91f05485f212a333dc4eb0d25a3f
Tom Vincent <git@tlvince.com>
2025-12-03 22:20:44 +0000

    fix(framework): apply SR latency +3us patch

--- hosts/framework.nix
+++ hosts/framework.nix
@@ -51,11 +51,11 @@
     # [x] linux-firmware git
     # [x] 6.16-rc6
     # [x] increase DCN35 SR latency by 1us
-    # [x] increase DCN35 SR latency by 2us
+    # [x] increase DCN35 SR latency by 2us - rarely with 80% zoom
     # [✓] increase DCN35 SR latency by 3us
+    # [✓] increase DCN35 SR latency by 4us
     # [✓] disable memory stutter (dcdebugmask=0x2)
     # [x] fixed scratch registers
-    # [✓] increase DCN35 SR latency by 4us
     #
     # See: https://community.frame.work/t/flickering-when-using-firefox-under-kde-wayland-on-ryzen-ai-300/69599
     # See: https://gitlab.freedesktop.org/drm/amd/-/issues/4451
@@ -63,10 +63,10 @@
     # labels: host:framework
     kernelPatches = [
       {
-        name = "drm/amd/display: Increase DCN35 SR enter/exit latency by 2us";
+        name = "drm/amd/display: Increase DCN35 SR enter/exit latency by 3us";
         patch = pkgs.fetchpatch {
-          url = "https://gitlab.freedesktop.org/-/project/4522/uploads/e213cbf551d73380e4603612e8beb1d4/0001-drm-amd-display-Increase-DCN35-SR-enter-exit-latency.patch";
-          sha256 = "sha256-ga7hb12sAgN5eNIDLusygJyTPz7kQlxXsNH2f9OAwJw=";
+          url = "https://gitlab.freedesktop.org/-/project/4522/uploads/daad272f57fc56572461d789bc105809/0001-drm-amd-display-Increase-DCN35-SR-latency-by-3us.patch";
+          sha256 = "sha256-84FgDHdqQ4OD4a5a3UoUGGC0Ip/oMug+t2FDpTHhzbc=";
         };
       }
     ];
