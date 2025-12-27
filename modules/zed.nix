0a72bd2a70d55c283803828a3592bd6068b0f3f0
Tom Vincent <git@tlvince.com>
2025-12-27 02:38:24 +0000

    feat(zed): zedless

--- modules/zed.nix
+++ modules/zed.nix
@@ -1,11 +1,18 @@
-{pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    codex-acp
-    nil
-    package-version-server
-    tailwindcss-language-server
-    vscode-json-languageserver
-    vtsls
-    zed-editor
-  ];
+{
+  pkgs,
+  zedless,
+  ...
+}: {
+  environment.systemPackages = with pkgs;
+    [
+      codex-acp
+      nil
+      package-version-server
+      tailwindcss-language-server
+      vscode-json-languageserver
+      vtsls
+    ]
+    ++ [
+      zedless.packages.${pkgs.stdenv.hostPlatform.system}.zedless
+    ];
 }
