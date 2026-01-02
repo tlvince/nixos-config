cacd379948b4314387a416abfaeaf5d49248932b
Tom Vincent <git@tlvince.com>
2026-01-02 15:48:43 +0000

    fix(lamma): cli qol

--- hosts/lamma/home.nix
+++ hosts/lamma/home.nix
@@ -3,7 +3,12 @@
   ...
 }:
 {
-  home.stateVersion = "25.11";
+  home = {
+    sessionVariables = {
+      ZSHZ_CMD = "j";
+    };
+    stateVersion = "25.11";
+  };
   manual = {
     html.enable = false;
     json.enable = false;
@@ -28,5 +33,19 @@
       };
     };
     htop.enable = true;
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+    };
+    zsh = {
+      enable = true;
+      initContent = ''
+        source ${pkgs.zsh-z}/share/zsh-z/zsh-z.plugin.zsh
+      '';
+    };
+
   };
 }
