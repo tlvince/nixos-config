eaf405709d1e1befc0e5aa3a0b7299092e68af2b
Tom Vincent <git@tlvince.com>
2026-01-12 09:00:37 +0000

    fix(llm-agents): drop antigravity

--- modules/llm-agents.nix
+++ modules/llm-agents.nix
@@ -8,36 +8,6 @@ let
 in
 {
   environment.systemPackages = [
-    (jail "antigravity" pkgs.antigravity (
-      with jail.combinators;
-      [
-        gpu
-        gui
-        network
-        open-urls-in-browser
-        (add-runtime ''
-          SHELL=/bin/sh
-          echo "root:x:0:0:System administrator:/root:$SHELL" > ~/.local/share/jail.nix/passwd
-          echo "$(id -un):x:$(id -u):$(id -g)::$HOME:$SHELL" >> ~/.local/share/jail.nix/passwd
-        '')
-        (add-pkg-deps (
-          with pkgs;
-          [
-            electron
-            nodejs
-            xdg-utils
-          ]
-        ))
-        (persist-home "antigravity")
-        # Add Wayland args
-        (set-env "NIXOS_OZONE_WL" "1")
-        # Prevent Electron forking the GUI
-        (set-argv [
-          "--wait"
-          (noescape "\"$@\"")
-        ])
-      ]
-    ))
     (jail "yolo-codex" pkgs.codex (
       with jail.combinators;
       [
