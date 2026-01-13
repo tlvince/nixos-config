d21debc7d9bf78504e4ba99f47927b2ad154a724
Tom Vincent <git@tlvince.com>
2026-01-13 22:11:50 +0000

    fix(host-common-nixos): uas quirks

--- modules/host-common-nixos.nix
+++ modules/host-common-nixos.nix
@@ -1,5 +1,12 @@
 { pkgs, ... }:
 {
+  boot.kernelParams = [
+    # Disable UAS for WD Elements to prevent controller resets
+    "usb-storage.quirks=1058:2621:u"
+    # Disable UAS on Seagate One Touch
+    "usb-storage.quirks=0bc2:ab64:u"
+  ];
+
   hardware.enableRedistributableFirmware = true;
 
   i18n.defaultLocale = "en_GB.UTF-8";
