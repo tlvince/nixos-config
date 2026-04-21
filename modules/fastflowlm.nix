667fdd14af5ccf3b613f9464420d6cc649848889
Tom Vincent <git@tlvince.com>
2026-04-21 17:28:27 +0100

    docs(fastflowlm): accel note

--- modules/fastflowlm.nix
+++ modules/fastflowlm.nix
@@ -10,6 +10,8 @@
 
     serviceConfig = {
       ExecStart = "${pkgsAmdgpu.fastflowlm}/bin/flm serve --host 127.0.0.1 --port 52625 --quiet";
+      # for /dev/accel/* (npu) access
+      # https://github.com/systemd/systemd/blob/b3d8fc43e9cb531d958c17ef2cd93b374bc14e8a/rules.d/50-udev-default.rules.in#L63
       SupplementaryGroups = [ "render" ];
       User = "flm";
 
