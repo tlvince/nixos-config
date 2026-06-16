dd1ee79b144ffb5496b01d6326c8d37fc074f4fa
Tom Vincent <git@tlvince.com>
2026-06-16 19:25:33 +0100

    fix(scripts): update device id

--- scripts.nix
+++ scripts.nix
@@ -1,9 +1,8 @@
 { pkgs, ... }:
 {
   notify = pkgs.writeScriptBin "notify" ''
-    DEVICE="mobile_app_pixel_7a"
     ${pkgs.jq}/bin/jq --null-input --arg title "$TITLE" --arg message "$MESSAGE" '{"title": $title, "message": $message}' | \
     ${pkgs.curl}/bin/curl -H "Authorization: Bearer $(<"$CREDENTIALS_DIRECTORY/notify")" --silent --output /dev/null --json @- \
-    "https://home-assistant.filo.uk/api/services/notify/$DEVICE"
+    "https://home-assistant.filo.uk/api/services/notify/mobile_app_pixel"
   '';
 }
