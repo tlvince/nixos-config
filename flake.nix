c4c842e07dd8afcdce14ba2f8b7cf560484c3dfa
Tom Vincent <git@tlvince.com>
2026-09-16 21:26:52 +0100

    feat(deepseek-harness): bump (#528)

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
     # Issue URL: https://github.com/tlvince/nixos-config/issues/512
     # See: https://github.com/NixOS/nixpkgs/pull/554081
     # labels: host:nea, module:dsh
-    nixpkgs-dsh.url = "github:tlvince/nixpkgs/8755f83ee3cda0218c9ad8f64bef5aa85a721c4e";
+    nixpkgs-dsh.url = "github:tlvince/nixpkgs/d3b152a9caac4f9a57e8452813791b8f181a4cc0";
     # TODO: Drop fastflowlm pin when PR is merged upstream
     # Issue URL: https://github.com/tlvince/nixos-config/issues/468
     # See: https://github.com/NixOS/nixpkgs/pull/513841
